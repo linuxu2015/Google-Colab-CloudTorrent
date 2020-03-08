07db5bf11ee332774d69f81b370b29c730fc238b
linuxu2015 <linuxu2015@gmail.com>
2020-03-08 22:35:29 +0800

    Update doneCMD.sh

--- res/scripts/doneCMD.sh
+++ res/scripts/doneCMD.sh
@@ -7,5 +7,5 @@ if [[ -d ${UP} ]]; then
 fi
 
 #Upload to Gdrive
-mkdir -p "/content/drive/Shared drives/linuxu/$(dirname "${CLD_PATH}")"
-mv "${UP}" "/content/drive/Shared drives/linuxu/${CLD_PATH}"
+mkdir -p "/content/drive/Shared drives/群友盘-家庭网络资源分享(电报ID:HomeNet6)/$(dirname "${CLD_PATH}")"
+mv "${UP}" "/content/drive/Shared drives/群友盘-家庭网络资源分享(电报ID:HomeNet6)/${CLD_PATH}"
