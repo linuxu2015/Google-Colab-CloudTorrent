9ca68b863a745529a62df189bf19036ec68f12f6
linuxu2015 <linuxu2015@gmail.com>
2020-03-08 22:49:15 +0800

    Update doneCMD.sh

--- res/scripts/doneCMD.sh
+++ res/scripts/doneCMD.sh
@@ -8,4 +8,4 @@ fi
 
 #Upload to Gdrive
 mkdir -p "/content/drive/Shared drives/群友盘-家庭网络资源分享(电报ID:HomeNet6)/VIDEO/专题合集/《在播剧追更》热播剧集首选/$(dirname "${CLD_PATH}")"
-mv "${UP}" "/content/drive/Shared drives/群友盘-家庭网络资源分享(电报ID:HomeNet6)/专题合集/《在播剧追更》热播剧集首选/${CLD_PATH}"
+mv "${UP}" "/content/drive/Shared drives/群友盘-家庭网络资源分享(电报ID:HomeNet6)/VIDEO/专题合集/《在播剧追更》热播剧集首选/${CLD_PATH}"
